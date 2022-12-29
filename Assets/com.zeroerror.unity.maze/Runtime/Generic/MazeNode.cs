accc01f64f55cde2d09d26788da3432435023bed
zh1668118834 <1668118834@qq.com>
2022-12-29 18:01:05 +0800

    <feature> add: 获取迷宫的可用节点信息

---
+++ Assets/com.zeroerror.unity.maze/Runtime/Generic/MazeNode.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace AutoMaze.Generic
+{
+
+    public struct MazeNode
+    {
+        
+        public int x;
+        public int y;
+        public bool isWalkable;
+
+    }
+
+}
--- Assets/com.zeroerror.unity.maze/Runtime/Maze.cs
+++ Assets/com.zeroerror.unity.maze/Runtime/Maze.cs
@@ -2,15 +2,61 @@ using System.Collections.Generic;
 using UnityEngine;
 using AutoMaze.Generic;
 
-public static class Maze_Easy
+struct MazeLink
+{
+    public Vector2 point1;
+    public Vector2 point2;
+}
+
+public static class Maze
 {
 
-    public static List<MazeLink> GenEastMaze(int width, int height)
+    public static MazeNode[,] GetMazeMap(int width, int height, Vector2 start)
+    {
+        var linkList = GetMazeLink_RPA(width, height, start);
+        int mapWidth = 2 * width + 1;
+        int mapHeight = 2 * height + 1;
+        MazeNode[,] map = new MazeNode[mapWidth, mapHeight];
+        ClearMap(map);
+        linkList.ForEach((link) =>
+        {
+            var p1 = link.point1;
+            var p2 = link.point2;
+            var x1 = (int)p1.x * 2 + 1;
+            var y1 = (int)p1.y * 2 + 1;
+            map[x1, y1].isWalkable = true;
+            var x2 = (int)p2.x * 2 + 1;
+            var y2 = (int)p2.y * 2 + 1;
+            map[x2, y2].isWalkable = true;
+
+            if (y1 == y2) map[Mathf.Min(x1, x2) + 1, y1].isWalkable = true;       // Horizontal break through.
+            if (x1 == x2) map[x1, Mathf.Min(y1, y2) + 1].isWalkable = true;       // Vertical break through.
+        });
+        return map;
+    }
+
+    static void ClearMap(MazeNode[,] map)
+    {
+        var mapWidth = map.GetLength(0);
+        var mapHeight = map.GetLength(0);
+        for (int i = 0; i < mapWidth; i++)
+        {
+            for (int j = 0; j < mapHeight; j++)
+            {
+                MazeNode node;
+                node.x = i;
+                node.y = j;
+                node.isWalkable = false;
+                map[i, j] = node;
+            }
+        }
+    }
+
+    static List<MazeLink> GetMazeLink_RPA(int width, int height, Vector2 start)
     {
         bool[,] searchArray = new bool[width, height];
         List<MazeLink> maze = new List<MazeLink>();
         Stack<Vector2> searchStack = new Stack<Vector2>();
-        Vector2 start = Vector2.zero;
         int x = (int)start.x;
         int y = (int)start.y;
         searchArray[x, y] = true;
@@ -24,7 +70,6 @@ public static class Maze_Easy
                 continue;
             }
 
-            // - 打通道路
             searchStack.Push(neighbour);
             searchArray[(int)neighbour.x, (int)neighbour.y] = true;
             maze.Add(new MazeLink { point1 = curPos, point2 = neighbour });
